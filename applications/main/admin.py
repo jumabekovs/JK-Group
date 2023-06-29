ab3339cbc38148ff5f257e6106b3e0cb60f0c1f2
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-06-29 19:55:11 +0600

    cloudinary video field

--- applications/main/admin.py
+++ applications/main/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django_summernote.utils import get_attachment_model
+
 from .models import Main
+from django.contrib.auth.models import Group, User
 
 
 admin.site.register(Main)
+admin.site.unregister(Group)
+admin.site.unregister(User)
+admin.site.unregister(get_attachment_model())
--- applications/main/models.py
+++ applications/main/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class Main(models.Model):
     title = models.CharField(verbose_name='название', max_length=256, unique=True)
     description = models.TextField(verbose_name='лозунг', default="Комплексный инжиниринг в Кыргызской Республике")
-    main_video = models.FileField(blank=True, null=True, verbose_name='видео главной страница', upload_to='jk-main-video')
+    main_video = CloudinaryField(resource_type="video", verbose_name='видео главной страница')
 
     def __str__(self):
         return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Главная страница"
+        verbose_name_plural = "Главная страница"
