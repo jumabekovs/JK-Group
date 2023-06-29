4497765badcfd3e79e2c34460f930ee2647ce962
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-06-29 20:21:14 +0600

    Docker

--- applications/company/models.py
+++ applications/company/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from applications.main.models import Main
+
+
+class Company(models.Model):
+    name = models.CharField(verbose_name="название", max_length=256)
+    group = models.ForeignKey(Main, related_name='companies', on_delete=models.CASCADE)
+    subtitle = models.CharField(verbose_name="подзаголовок", max_length=999)
+    description = models.TextField()
--- applications/main/models.py
+++ applications/main/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 class Main(models.Model):
     title = models.CharField(verbose_name='название', max_length=256, unique=True)
     description = models.TextField(verbose_name='лозунг', default="Комплексный инжиниринг в Кыргызской Республике")
-    main_video = CloudinaryField(resource_type="video", verbose_name='видео главной страница')
+    main_video = CloudinaryField(resource_type="video", verbose_name='видео главной страница', null=True, blank=True)
 
     def __str__(self):
         return f"{self.title}"
