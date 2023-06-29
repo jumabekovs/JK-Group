e02bf73c7812298103109f5365a5ebbeaa15d0ce
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-06-29 19:28:35 +0600

    jk-group app

--- applications/main/models.py
+++ applications/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Main(models.Model):
+    title = models.CharField(verbose_name='название', max_length=256, unique=True)
+    description = models.TextField(verbose_name='лозунг', default="Комплексный инжиниринг в Кыргызской Республике")
+    main_video = models.FileField(blank=True, null=True, verbose_name='видео главной страница', upload_to='jk-main-video')
+
+    def __str__(self):
+        return f"{self.title}"
