1f27951e6cdc4cf53ce20726e3e45eb94cbed62e
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-07-03 02:23:13 +0600

    finally

--- source/settings.py
+++ source/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -174,11 +174,10 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000','https://localhost:3000', "https://jk-group-production.up.railway.app"]
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://localhost:3000', "https://jk-group-production.up.railway.app"]
-CORS_ORIGIN_WHITELIST = ['http://localhost:3000', ]
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000','https://localhost:3000', "https://jk-group-production.up.railway.app"]
+
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dn17ltczr',
