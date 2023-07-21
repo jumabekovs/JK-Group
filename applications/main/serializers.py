2b56687b4c447a7746067fd5c2779bb51115c487
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-07-21 16:49:52 +0600

    main_video

--- applications/main/serializers.py
+++ applications/main/serializers.py
@@ -7,19 +7,23 @@ from ..staff.models import Team
 
 
 class MainSerializer(serializers.ModelSerializer):
-    # video_url = serializers.SerializerMethodField()
-
     class Meta:
         model = Main
-        exclude = ("main_video",)
+        fields = "__all__"
 
-    # def get_video_url(self, main):
-    #     request = self.context.get('request')
-    #     video_url = main.main_video.url
-    #     return request.build_absolute_uri(video_url)
+    def get_video_url(self, obj):
+        if obj.main_video:
+            url = obj.main_video.url
+            requests = self.context.get("request")
+            if requests is not None:
+                url = requests.build_absolute_uri(url)
+        else:
+            url = ""
+        return url
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
+        representation['main_video'] = self.get_video_url(instance)
         representation['lines'] = LineListSerializer(Line.objects.all(), many=True).data
         representation['statistics'] = {'Projects': Project.objects.all().count(), 'Lines of Bussiness':
             Line.objects.all().count(), 'Team members': Team.objects.all().count()}
