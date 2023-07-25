2db2e4fb1788ec65c41bbff3679e35cfeb9d7063
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-07-25 14:19:16 +0600

    removed fields

--- applications/history/models.py
+++ applications/history/models.py
@@ -4,7 +4,8 @@ from applications.company.models import Company
 
 
 class History(models.Model):
-    company = models.ForeignKey(Company, verbose_name='компания', related_name='history', on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, verbose_name='компания', related_name='history', on_delete=models.DO_NOTHING,
+                                null=True)
     title = models.CharField(verbose_name="заголовок", max_length=556)
     sub_title = models.CharField(verbose_name="подзаголовок", max_length=996, blank=True, null=True)
     description = models.TextField(verbose_name='описание')
@@ -20,16 +21,14 @@ class History(models.Model):
 
 
 class ExtraFields(models.Model):
-    history = models.ForeignKey(History, related_name='extrafields', on_delete=models.CASCADE, null=True)
-    sub_title = models.CharField(verbose_name='подзаголовок', max_length=999, blank=True, null=True)
-    description = models.TextField(verbose_name='описание', blank=True, null=True)
+    history = models.ForeignKey(History, related_name='extrafields', on_delete=models.DO_NOTHING, null=True)
     picture = models.ImageField(verbose_name='фотография', upload_to='history_images',
                                 blank=True, null=True)
 
     def __str__(self):
-        return f"{self.sub_title}"
+        return f"{self.history}"
 
     class Meta:
-        verbose_name = 'экстра поле'
-        verbose_name_plural = 'экстра поля'
+        verbose_name = 'Экстра картинки'
+        verbose_name_plural = 'Экстра картинки'
 
