578e7bb3af399a5996e9a088961e82305be5ce33
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-07-25 14:21:53 +0600

    fix bugs

--- applications/history/admin.py
+++ applications/history/admin.py
@@ -8,9 +8,6 @@ class ExtraFieldsAdmin(SummernoteModelAdminMixin, admin.TabularInline):
     model = ExtraFields
     max_num = 6
     extra = 1
-    summernote_fields = ('sub_title_ru', 'sub_title_ky', 'sub_title_en', 'description_ru', 'description_ky',
-                         'description_en', )
-    exclude = ('sub_title', 'description')
 
 
 class HistoryAdmin(SummernoteModelAdmin, TranslationAdmin):
--- applications/history/translation.py
+++ applications/history/translation.py
@@ -5,8 +5,3 @@ from .models import History, ExtraFields
 @register(History)
 class HistoryTranslationOptions(TranslationOptions):
     fields = ('title', 'sub_title', 'description')
-
-
-@register(ExtraFields)
-class ExtraFieldsTranslationOptions(TranslationOptions):
-    fields = ('sub_title', 'description')
