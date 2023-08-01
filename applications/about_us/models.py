7faf95feca25a74d663b74ac0edfea72c81e4865
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-08-01 11:16:36 +0600

    added picture

--- applications/about_us/models.py
+++ applications/about_us/models.py
@@ -5,6 +5,8 @@ class AboutUs(models.Model):
     title = models.CharField(verbose_name='название', max_length=256, unique=True)
     subtitle = models.CharField(verbose_name='подзоголовок', max_length=556, blank=True, null=True)
     description = models.TextField(verbose_name='текст')
+    picture = models.ImageField(verbose_name='фотография', upload_to='about_us_image',
+                                blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} - {self.description}"
